a128f97f4ae9c92c2210136cf613c765daaa501e
farid <faridfth1994@yahoo.com>
2023-06-13 16:55:53 +0330

    add buttons

--- calculator.py
+++ calculator.py
@@ -15,6 +15,17 @@ class calculator(tk.Tk):
         button_frame = tk.Frame(self)
         button_frame.grid(row=1, column=0, columnspan=4, padx=10, pady=10)
 
-        
+        self.create_button("1", button_frame, 0, 0, lambda: self.add_number(1))
+        self.create_button("2", button_frame, 0, 1, lambda: self.add_number(2))
+        self.create_button("3", button_frame, 0, 2, lambda: self.add_number(3))
+        self.create_button("4", button_frame, 1, 0, lambda: self.add_number(4))
+        self.create_button("5", button_frame, 1, 1, lambda: self.add_number(5))
+        self.create_button("6", button_frame, 1, 2, lambda: self.add_number(6))
+        self.create_button("7", button_frame, 2, 0, lambda: self.add_number(7))
+        self.create_button("8", button_frame, 2, 1, lambda: self.add_number(8))
+        self.create_button("9", button_frame, 2, 2, lambda: self.add_number(9))
+        self.create_button("0", button_frame, 3, 1, lambda: self.add_number(0))
+
+
 
         
